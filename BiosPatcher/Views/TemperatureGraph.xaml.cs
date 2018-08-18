bc931dc06b0275af8ab3be7ff2994abab3ecb4ce
saltukkos <saltukkos@gmail.com>
2018-08-19 03:56:05 +0600

    Add dynamic descriptions on graphic

--- BiosPatcher/Views/TemperatureGraph.xaml.cs
+++ BiosPatcher/Views/TemperatureGraph.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
 using JetBrains.Annotations;
 
@@ -128,6 +130,57 @@ namespace BiosPatcher.Views
         private void OnCanvasResize(object sender, SizeChangedEventArgs e)
         {
             RedrawGraph();
+        }
+
+        private void ResetHighlight()
+        {
+            Level0Graph.Fill = (Brush) FindResource("Color0");
+            Level1Graph.Fill = (Brush) FindResource("Color1");
+            Level2Graph.Fill = (Brush) FindResource("Color2");
+            Level3Graph.Fill = (Brush) FindResource("Color3");
+            Level4Graph.Fill = (Brush) FindResource("Color4");
+            Level5Graph.Fill = (Brush) FindResource("Color5");
+        }
+
+        private void OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            ResetHighlight();
+            DescriptionLabel.Content = Description;
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            ResetHighlight();
+            var position = e.GetPosition(Canvas);
+
+            if (position.X / Canvas.ActualWidth + position.Y / Canvas.ActualHeight < 1)
+            {
+                DescriptionLabel.Content = Description;
+                return;
+            }
+
+            var graphs = new[] {Level0Graph, Level1Graph, Level2Graph, Level3Graph, Level4Graph, Level5Graph};
+            var levels = new[] {Level1, Level2, Level3, Level4, Level5};
+            var levelUnderCursor = 5;
+            for (int i = 0; i < levels.Length; ++i)
+            {
+                if (GetWidthForTemperature(levels[i]) > position.X)
+                {
+                    levelUnderCursor = i;
+                    break;
+                }
+            }
+
+            graphs[levelUnderCursor].Fill = new SolidColorBrush(Color.FromRgb(0, 255, 200));
+           
+            if (levelUnderCursor > 0)
+            {
+                DescriptionLabel.Content = $"{Description}{Environment.NewLine}Fan speed {levelUnderCursor}, at {levels[levelUnderCursor - 1]} °C";
+            }
+            else
+            {
+                DescriptionLabel.Content = $"{Description}{Environment.NewLine}Fan turned off";
+            }
         }
     }
 }
